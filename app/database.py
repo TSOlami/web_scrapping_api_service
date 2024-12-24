1b7d18fc1b022015473f810eadfffc06efad49e6
TSOlami <tijani.saheed@yahoo.com>
2024-12-24 14:59:23 +0100

    Add logging for successful database connection

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy.orm import Session
 from .models import SessionLocal
+import logging
+
 
 # Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
         yield db
+        logging.info("Database connection successful")
     finally:
         db.close()
